3a7c8fd610ece4e016ff74be7aada00fbc80336a
Nick Schonning <nschonni@gmail.com>
2013-05-22 16:02:34 -0400

    Fix paths for moved spec files

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-NuGet.exe pack WET-BOEW.Core.nuspec
-NuGet.exe pack WET-BOEW.Theme.GCWU-FEGC.nuspec
+NuGet.exe pack WET-BOEW.Core\WET-BOEW.Core.nuspec
+NuGet.exe pack WET-BOEW.Theme.GCWU-FEGC\WET-BOEW.Theme.GCWU-FEGC.nuspec
