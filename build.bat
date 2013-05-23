de6c4676d47446350fe2ca74046801c536ddcf8e
Nick Schonning <nschonni@gmail.com>
2013-05-23 00:24:46 -0400

    Add CLF2 theme spec file
    Made clear that theme was transitional only

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 NuGet.exe pack WET-BOEW.Core\WET-BOEW.Core.nuspec
 NuGet.exe pack WET-BOEW.Theme.GCWU-FEGC\WET-BOEW.Theme.GCWU-FEGC.nuspec
 NuGet.exe pack WET-BOEW.Theme.Intranet\WET-BOEW.Theme.Intranet.nuspec
+NuGet.exe pack WET-BOEW.Theme.CLF2-NSI2\WET-BOEW.Theme.CLF2-NSI2.nuspec
