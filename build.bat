03c284a50c01eedb6dff873aa9b1e85b2981ae4a
Nick Schonning <nschonni@gmail.com>
2013-05-23 00:15:39 -0400

    Add Intranet theme spec file

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 NuGet.exe pack WET-BOEW.Core\WET-BOEW.Core.nuspec
 NuGet.exe pack WET-BOEW.Theme.GCWU-FEGC\WET-BOEW.Theme.GCWU-FEGC.nuspec
+NuGet.exe pack WET-BOEW.Theme.Intranet\WET-BOEW.Theme.Intranet.nuspec
